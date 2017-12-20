b1ed9f05a7f5d4fb20a3908a7b2a1b691cbb8545
sdsgisd <c0652@c0652-MacBook-Air.local>
2017-12-20 09:47:14 +0200

    Modify parameters

--- OpenCL_Examples/kernel.cl
+++ OpenCL_Examples/kernel.cl
@@ -1,12 +1,12 @@
 __kernel
-void add_array_opencl(__global float* input1,__global float* input2,__global float* output, const int n)
+void add_array_opencl(__global float* input1,__global float* input2,__global float* output, const int num_elements)
 {
     int index = get_global_id(0);
     
     int iterations=30;
     for(int j=0;j<iterations;++j){
-        if (index < n) {
+        //if (index < num_elements) {
             output[index] += input1[index]+input2[index];
-        }
+        //}
     }
 }
--- OpenCL_Examples/main.cpp
+++ OpenCL_Examples/main.cpp
@@ -33,7 +33,7 @@ void measure_runtime(Func &target_func,const unsigned iteration,Args...args ){
 };
 
 
-void EC(cl_int result, const char *title)
+void ErrorChecker(cl_int result, const char *title)
 {
     if (result != CL_SUCCESS) {
         std::cout << "Error: " << title << "(" << result << ")\n";
@@ -42,7 +42,7 @@ void EC(cl_int result, const char *title)
 
 
 cl_int err = CL_SUCCESS;
-void EC2(const char *title)
+void EerrorChecker(const char *title)
 {
     if (err != CL_SUCCESS) {
         std::cout << "Error: " << title << "(" << err << ")\n";
@@ -76,13 +76,13 @@ void add_array_cpu(float * input1, float *input2,float*output,unsigned num_eleme
     
 }
 
-void perform_via_cpu(float * array1, float *array2,float*result_array_normal,unsigned n){
+void perform_via_cpu(float * array1, float *array2,float*result_array_normal,unsigned num_elements){
     
-    measure_runtime(add_array_cpu,1, array1,array2,result_array_normal,n);
+    measure_runtime(add_array_cpu,1, array1,array2,result_array_normal,num_elements);
     
     const bool display_computational_results=false;
     if(display_computational_results){
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num_elements; i++) {
             std::cout << result_array_normal[i] << ", ";
         }
         std::cout << "\n";
@@ -91,90 +91,117 @@ void perform_via_cpu(float * array1, float *array2,float*result_array_normal,uns
     
 }
 
-int perform_via_opencl(float * array1, float *array2,float*result_array_cl,unsigned n){
-    // Obtain platform.
-    cl_platform_id platforms[PLATFORM_MAX];
-    cl_uint platformCount;
-    EC(clGetPlatformIDs(PLATFORM_MAX, platforms, &platformCount), "clGetPlatformIDs");
-    if (platformCount == 0) {
-        std::cerr << "No platform.\n";
-        return EXIT_FAILURE;
-    }
+int perform_via_opencl(float * array1, float *array2,float*result_array_cl,unsigned num_elements){
+
     
-    // Display platform information
-    for (int i = 0; i < platformCount; i++) {
-        char vendor[100] = {0};
-        char version[100] = {0};
-        EC(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(vendor), vendor, nullptr), "clGetPlatformInfo");
-        EC(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, sizeof(version), version, nullptr), "clGetPlatformInfo");
-        std::cout << "Platform id: " << platforms[i] << ", Vendor: " << vendor << ", Version: " << version << "\n";
-    }
+
+
     
-    // Obtain devices
-    cl_device_id devices[DEVICE_MAX];
-    cl_uint deviceCount;
-    EC(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, DEVICE_MAX, devices, &deviceCount), "clGetDeviceIDs");
-    if (deviceCount == 0) {
-        std::cerr << "No device.\n";
-        return EXIT_FAILURE;
-    }
-    
-    // Display devices information
-    std::cout << deviceCount << " device(s) found.\n";
-    for (int i = 0; i < deviceCount; i++) {
-        char name[100] = {0};
-        size_t len;
-        EC(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, &len), "clGetDeviceInfo");
-        std::cout << "Device id: " << i << ", Name: " << name << "\n";
-    }
-    
-    // Make context
-    cl_context ctx = clCreateContext(nullptr, 1, devices, nullptr, nullptr, &err);
-    EC2("clCreateContext");
-    
-    // Read precompiled opencl source
-    const char* bitcode_path = "OpenCL/kernel.cl.gpu_64.bc";
-    
-    size_t len = strlen(bitcode_path);
-    cl_program program = clCreateProgramWithBinary(ctx, 1, devices, &len, (const unsigned char**)&bitcode_path, nullptr, &err);
-    EC2("clCreateProgramWithBinary");
-    
-    // build
-    EC(clBuildProgram(program, 1, devices, nullptr, nullptr, nullptr), "clBuildProgram");
-    
-    // make kernel
-    cl_kernel kernel = clCreateKernel(program, "add_array_opencl", &err);
-    EC2("clCreateKernel");
-    
-    // allocate device memory
-    cl_mem device_mem1 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * n, array1, &err);
-    
-    cl_mem device_mem2 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * n, array2, &err);
-    
-    cl_mem device_mem_result = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * n, result_array_cl, &err);
-    EC2("clCreateBuffer");
-    
-    // set parameters to kernels
-    EC(clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_mem1), "clSetKernelArg");
-    
-    EC(clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_mem2), "clSetKernelArg");
-    
-    EC(clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_mem_result), "clSetKernelArg");
-    
-    
-    EC(clSetKernelArg(kernel, 3, sizeof(int), &n), "clSetKernelArg");
-    
-    // make command queue
-    cl_command_queue q = clCreateCommandQueue(ctx, devices[0], 0, &err);
-    EC2("clCreateCommandQueue");
+
     
     auto exec_kernel=[&](){
+        // Obtain platform.
+        cl_platform_id platforms[PLATFORM_MAX];
+        cl_uint platformCount;
+        ErrorChecker(clGetPlatformIDs(PLATFORM_MAX, platforms, &platformCount), "clGetPlatformIDs");
+        if (platformCount == 0) {
+            std::cerr << "No platform.\n";
+            exit(EXIT_FAILURE);
+        }
+        
+        // Display platform information
+        for (int i = 0; i < platformCount; i++) {
+            char vendor[100] = {0};
+            char version[100] = {0};
+            ErrorChecker(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(vendor), vendor, nullptr), "clGetPlatformInfo");
+            ErrorChecker(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, sizeof(version), version, nullptr), "clGetPlatformInfo");
+            std::cout << "Platform id: " << platforms[i] << ", Vendor: " << vendor << ", Version: " << version << "\n";
+        }
+        
+
+        
+        // Obtain devices
+        cl_device_id devices[DEVICE_MAX];
+        cl_uint deviceCount;
+        ErrorChecker(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, DEVICE_MAX, devices, &deviceCount), "clGetDeviceIDs");
+        if (deviceCount == 0) {
+            std::cerr << "No device.\n";
+            exit(EXIT_FAILURE);
+        }
+    
+        
+        // Display devices information
+        std::cout << deviceCount << " device(s) found.\n";
+        for (int i = 0; i < deviceCount; i++) {
+            char name[100] = {0};
+            size_t len;
+            ErrorChecker(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, &len), "clGetDeviceInfo");
+            std::cout << "Device id: " << i << ", Name: " << name << "\n";
+        }
+
+        
+        // Make context
+        cl_context ctx = clCreateContext(nullptr, 1, devices, nullptr, nullptr, &err);
+        EerrorChecker("clCreateContext");
+        
+        // Read precompiled opencl source
+        const char* bitcode_path = "OpenCL/kernel.cl.gpu_64.bc";
+        
+        size_t len = strlen(bitcode_path);
+        cl_program program = clCreateProgramWithBinary(ctx, 1, devices, &len, (const unsigned char**)&bitcode_path, nullptr, &err);
+        EerrorChecker("clCreateProgramWithBinary");
+        
+        // build
+        ErrorChecker(clBuildProgram(program, 1, devices, nullptr, nullptr, nullptr), "clBuildProgram");
+        
+        // make kernel
+        cl_kernel kernel = clCreateKernel(program, "add_array_opencl", &err);
+        EerrorChecker("clCreateKernel");
+        
+        // allocate device memory
+        cl_mem device_mem1 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * num_elements, array1, &err);
+        
+        cl_mem device_mem2 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * num_elements, array2, &err);
+        
+        cl_mem device_mem_result = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * num_elements, result_array_cl, &err);
+        EerrorChecker("clCreateBuffer");
+        
+        // set parameters to kernels
+        ErrorChecker(clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_mem1), "clSetKernelArg");
+        
+        ErrorChecker(clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_mem2), "clSetKernelArg");
+        
+        ErrorChecker(clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_mem_result), "clSetKernelArg");
+        
+        
+        ErrorChecker(clSetKernelArg(kernel, 3, sizeof(int), &num_elements), "clSetKernelArg");
+        
+        // make command queue
+        cl_command_queue q = clCreateCommandQueue(ctx, devices[0], 0, &err);
+        EerrorChecker("clCreateCommandQueue");
+        
         // execute kernels
-        size_t global = n;
-        EC(clEnqueueNDRangeKernel(q, kernel, 1, nullptr, &global, nullptr, 0, nullptr, nullptr), "clEnqueueNDRangeKernel");
+        size_t global = num_elements;
+        ErrorChecker(clEnqueueNDRangeKernel(q, kernel, 1, nullptr, &global, nullptr, 0, nullptr, nullptr), "clEnqueueNDRangeKernel");
         
         // Read the results
-        EC(clEnqueueReadBuffer(q, device_mem_result, CL_TRUE, 0, sizeof(float) * n, result_array_cl, 0, nullptr, nullptr), "clEnqueueReadBuffer");
+        ErrorChecker(clEnqueueReadBuffer(q, device_mem_result, CL_TRUE, 0, sizeof(float) * num_elements, result_array_cl, 0, nullptr, nullptr), "clEnqueueReadBuffer");
+        
+        
+        // Release command queue
+        ErrorChecker(clReleaseCommandQueue(q), "clReleaseCommandQueue");
+        
+        // Release memory of the devices
+        ErrorChecker(clReleaseMemObject(device_mem1), "clReleaseMemObject");
+        
+        // Release kernel
+        ErrorChecker(clReleaseKernel(kernel), "clReleaseKernel");
+        
+        // Release program
+        ErrorChecker(clReleaseProgram(program), "clReleaseProgram");
+        
+        // Release context
+        ErrorChecker(clReleaseContext(ctx), "clReleaseContext");
         
         
     };
@@ -183,30 +210,15 @@ int perform_via_opencl(float * array1, float *array2,float*result_array_cl,unsig
     measure_runtime(exec_kernel,1);
     
 
-    
     const bool display_computational_results=false;
     if(display_computational_results){
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num_elements; i++) {
             std::cout << result_array_cl[i] << ", ";
         }
         std::cout << "\n";
         
     }
-    
-    // Release command queue
-    EC(clReleaseCommandQueue(q), "clReleaseCommandQueue");
-    
-    // Release memory of the devices
-    EC(clReleaseMemObject(device_mem1), "clReleaseMemObject");
-    
-    // Release kernel
-    EC(clReleaseKernel(kernel), "clReleaseKernel");
-    
-    // Release program
-    EC(clReleaseProgram(program), "clReleaseProgram");
-    
-    // Release context
-    EC(clReleaseContext(ctx), "clReleaseContext");
+
     
     return EXIT_SUCCESS;
 }
@@ -215,22 +227,22 @@ int perform_via_opencl(float * array1, float *array2,float*result_array_cl,unsig
 int main(int argc, const char * argv[])
 {
     // Prepare data
-    int n = 100000000;
+    int num_elements = 100000000;
     float *array1,*array2,*result_array_cl,*result_array_normal;
-    array1=new float[n];
-    array2=new float[n];
-    result_array_cl=new float[n];
-    result_array_normal=new float[n];
+    array1=new float[num_elements];
+    array2=new float[num_elements];
+    result_array_cl=new float[num_elements];
+    result_array_normal=new float[num_elements];
     
-    prepare_arrays(array1,array2,result_array_normal,n);
-    prepare_arrays(array1,array2,result_array_cl,n);
+    prepare_arrays(array1,array2,result_array_normal,num_elements);
+    prepare_arrays(array1,array2,result_array_cl,num_elements);
     
     //Compute two ways, straightforward v.s. opencl.
     std::cout<<"[Straightforwad Computation via CPU]"<<std::endl;
-    perform_via_cpu(array1,array2,result_array_normal,n);
+    perform_via_cpu(array1,array2,result_array_normal,num_elements);
     std::cout<<std::endl;
     std::cout<<"[Parallel Computation via OpenCL]"<<std::endl;
-    perform_via_opencl(array1,array2,result_array_cl,n);
+    perform_via_opencl(array1,array2,result_array_cl,num_elements);
     
     
     
